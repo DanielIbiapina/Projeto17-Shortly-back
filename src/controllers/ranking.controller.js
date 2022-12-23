e18d71c236f3848e700aa8923ee544948c5f30b8
DanielIbiapina <daniel18.ibiapinabarros@gmail.com>
2022-12-23 17:48:21 -0300

    deploy

--- src/controllers/ranking.controller.js
+++ src/controllers/ranking.controller.js
@@ -3,7 +3,7 @@ import { connectionDB } from "../database/db.js";
 export async function getRanking(req, res){
 
     try{
-        const ranking = await connectionDB.query('SELECT * FROM urls LIMIT 10');
+        const ranking = await connectionDB.query('SELECT * FROM users LIMIT 10');
         res.send(ranking.rows)
     }catch (err) {
         res.status(500).send(err.message);
